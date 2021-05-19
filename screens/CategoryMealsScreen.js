6524d51c722c64005edd98b8b850a75b89c702bf
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-05-19 23:44:49 +0500

    Update CategoryMealsScreen.js

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -3,14 +3,19 @@ import {View,Text,StyleSheet} from 'react-native';
 
 function CategoryMealsScreen() {
     return (
-        <View>
+        <View style={styles.screen} >
             
         </View>
     )
 }
 
 styles=StyleSheet.create({
-
+    screen:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+    }
 });
+
 
 export default CategoryMealsScreen
