b69e28dfa93e984caa440efe3bd35b1c9e97fb33
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-05-19 23:44:32 +0500

    Update CategoriesScreen.js

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -3,14 +3,18 @@ import {View,Text,StyleSheet} from 'react-native';
 
 function CategoriesScreen() {
     return (
-        <View>
+        <View style={styles.screen} >
             
         </View>
     )
 }
 
 styles=StyleSheet.create({
-
+    screen:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+    }
 });
 
 export default CategoriesScreen
