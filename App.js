995c5337bcb03aa4855a50bb96f3ed1e62b50ffa
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-05-21 01:20:36 +0500

    app loading

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React,{useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import MealsNavigator from './navigation/MealsNavigator'
+import AppLoading from 'expo-app-loading';
 
 const fetchFonts=()=>{
  return Font.loadAsync({
